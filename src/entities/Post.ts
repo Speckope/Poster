acdfcb46ef4b82ab2e701cd2c0eef899738e65b2
Szymon Kucharski <speckope@gmail.com>
2021-01-18 14:07:28 +0100

    CRUD with GraphQL, microorm and PostgreSql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,20 +1,25 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
 
-// @Entity tells micro-orm that it's a db table
+// @ObjectType tells GraphQL that is a type. We can stach decorators btw!
+@ObjectType()
 @Entity()
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
-  // createdAt and updatedAt are just standard fields that are good to have!
+  // @Field() is exposing it to our graphQL schema.
+  // () => String explicitly sets a type for GraphQL
+  @Field(() => String)
   @Property({ type: 'date' })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() }) // onUpdate its a hook that fires on update!
   updatedAt = new Date();
 
-  // @Property() decorates it. Without it its just a field in a class!
-  // With its a column in db!
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import microConfig from './mikro-orm.config';
@@ -5,6 +6,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -14,12 +16,17 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       // By defaul validation uses class validtors package
       validate: false,
     }),
+    // context is a special object that's accessible by all resolvers
+    context: () => ({ em: orm.em }),
   });
 
+  // This creates graphQL endpoint for us on express!
+  // Now when we go to localhost:4000/grahql we have a graphql playground!!! :D
+  // We can test our queries there.
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
